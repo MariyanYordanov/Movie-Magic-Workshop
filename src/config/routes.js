20f89f4820394f7256c12932a92043705ac0dd62
Mariyan Yordanov <marsrewq@gmail.com>
2024-07-29 16:49:58 +0300

    Add search functionality to catalog

--- src/config/routes.js
+++ src/config/routes.js
@@ -3,6 +3,7 @@ const { home, details } = require('../controllers/catalog');
 const { about } = require('../controllers/about');
 const { createGet } = require('../controllers/movie');
 const { notFound } = require('../controllers/404');
+const { search } = require('../controllers/catalog');
 
 const router = Router();
 
@@ -10,6 +11,7 @@ router.get('/', home);
 router.get('/about', about);
 router.get('/details/:id', details);
 router.get('/create', createGet);
+router.get('/search', search);
 
 router.all('*', notFound);
 
--- src/controllers/catalog.js
+++ src/controllers/catalog.js
@@ -4,5 +4,8 @@ module.exports = {
     },
     details: (req, res) => {
         res.render('details', {title: 'Details Page'});
+    },
+    search: (req, res) => {
+        res.render('search', {title: 'Search Page'});
     }
 };
