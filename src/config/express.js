3b048dca3ee3aba1b7d2054e885d00599c8a894d
Mariyan Yordanov <marsrewq@gmail.com>
2024-07-29 10:40:43 +0300

    Fix import formatting and variable names in routes and express configuration

--- src/config/express.js
+++ src/config/express.js
@@ -1,8 +1,8 @@
-const { urlencoded, static: staticHandler} = require('express');
+const { urlencoded, static: handler} = require('express');
 
 function expressConfig(app) {
     app.use(urlencoded({ extended: true }));
-    app.use('/static',staticHandler('static'));
+    app.use('/static', handler('static'));
 };
 
 module.exports = { expressConfig };
