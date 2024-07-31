90914b2e6226de01621a4a1525f28eead1eff20f
Mariyan Yordanov <marsrewq@gmail.com>
2024-07-31 03:37:40 +0300

    Add error handling for create movie form

--- src/controllers/movie.js
+++ src/controllers/movie.js
@@ -5,6 +5,22 @@ module.exports = {
         res.render('create', {title: 'Create Page'});
     },
     createPost: async (req, res) => {
+
+        const errors = {
+            title: !req.body.title,
+            genre: !req.body.genre,
+            director: !req.body.director,
+            year: !req.body.year,
+            imageURL: !req.body.imageURL,
+            rating: !req.body.rating,
+            description: !req.body.description
+        };
+        
+        if(Object.values(errors).includes(true)){
+            res.render('create', {title: 'Create Error Page', errors, movie: req.body});
+            return;
+        }
+
         const movie = await createMovie(req.body);
         res.redirect('/details/' + movie.id);
     }
