2b54308a2672bc32d70f71e98548f764b324d200
Mariyan Yordanov <marsrewq@gmail.com>
2024-07-31 17:56:39 +0300

    Refactor movie.js file

--- src/services/movie.js
+++ src/services/movie.js
@@ -1,104 +1,97 @@
-const fs = require('fs/promises');
-const { Movie } = require('../models/Movie');
+const fs = require("fs/promises");
+const { Movie } = require("../models/Movie");
 
-const filePath = './data/database.json';
+const filePath = "./data/database.json";
 
 async function readFileAsync() {
-
-    try {
-
-        const data = await fs.readFile(filePath, 'utf-8');
-        return JSON.parse(data.toString());
-
-    } catch (err) {
-
-        console.error(err);
-        return [];
-    }
+  try {
+    const data = await fs.readFile(filePath, "utf-8");
+    return JSON.parse(data.toString());
+  } catch (err) {
+    console.error(err);
+    return [];
+  }
 }
 
 async function writeFileAsync(data) {
-
-    await fs.writeFile(filePath, JSON.stringify(data));
-};
+  await fs.writeFile(filePath, JSON.stringify(data));
+}
 
 async function toMovieModel(data) {
+  const movie = new Movie();
 
-    const movie = new Movie();
+  movie.id = data.id;
+  movie.title = data.title;
+  movie.genre = data.genre;
+  movie.director = data.director;
+  movie.year = data.year;
+  movie.imageURL = data.imageURL;
+  movie.rating = data.rating;
+  movie.description = data.description;
 
-    movie.id = data.id;
-    movie.title = data.title;
-    movie.genre = data.genre;
-    movie.director = data.director;
-    movie.year = data.year;
-    movie.imageURL = data.imageURL;
-    movie.rating = data.rating;
-    movie.description = data.description;
-
-    return movie;
+  return movie;
 }
 
 async function getAllMovies() {
-
-    const movies = await readFileAsync();
-    const movieModels = movies.map(x => toMovieModel(x).join(''));
-
-    return movieModels; 
+  const allMovies = await readFileAsync();
+  const movies = allMovies.map((x) => toMovieModel(x));
+  return movies;
 }
 
 async function getMovieById(id) {
+  const movies = await readFileAsync();
+  const movie = movies.find((x) => x.id == id);
 
-    const movies = await readFileAsync();
-    const movie = movies.find(x => x.id == id);
-    
-    return movie ? toMovieModel(movie) : movie;
+  return movie ? toMovieModel(movie) : movie;
 }
 
 async function createMovie(data) {
-    
-    const movie = new Movie(); 
-    const id = await uuid();
+  const movie = new Movie();
+  const id = await uuid();
 
-    movie.id = id;
-    movie.title = data.title;
-    movie.genre = data.genre;
-    movie.director = data.director;
-    movie.year = data.year;
-    movie.imageURL = data.imageURL;
-    movie.rating = data.rating;
-    movie.description = data.description;
+  movie.id = id;
+  movie.title = data.title;
+  movie.genre = data.genre;
+  movie.director = data.director;
+  movie.year = data.year;
+  movie.imageURL = data.imageURL;
+  movie.rating = data.rating;
+  movie.description = data.description;
 
-    const movies = await readFileAsync();
-    movies.push(movie);
-    await writeFileAsync(movies);
+  const movies = await readFileAsync();
+  movies.push(movie);
+  await writeFileAsync(movies);
 
-    return toMovieModel(movie);
+  return toMovieModel(movie);
 }
 
 async function uuid() {
-    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-        const r = Math.random() * 16 | 0;
-        const v = c == 'x' ? r : (r & 0x3 | 0x8);
-        return v.toString(16);
-    });
+  return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function (c) {
+    const r = (Math.random() * 16) | 0;
+    const v = c == "x" ? r : (r & 0x3) | 0x8;
+    return v.toString(16);
+  });
 }
 
-async function searchAsync(title, genre, year) {
-    
-    const allMovies = await readFileAsync();
+async function searchAsync(query) {
+  const movies = await readFileAsync();
+  const url = new URLSearchParams(query);
+  const title = url.get("title");
+  const genre = url.get("genre");
+  const year = url.get("year");
 
-    const filteredMovies = allMovies.filter(x => 
-        x.title.toLowerCase().includes(title.toLowerCase()) ||
-        x.genre.toLowerCase().includes(genre.toLowerCase()) ||
-        x.year.toString().includes(year.toString())
-    );
-    
-    return filteredMovies.map(x => toMovieModel(x).join(''));
+  const result = movies.filter(
+    (x) =>
+      x.title.includes(title) ||
+      x.genre.includes(genre) ||
+      x.year.includes(year)
+  );
+  return result.map((x) => toMovieModel(x));
 }
 
-module.exports = { 
-    getAllMovies,
-    getMovieById,
-    createMovie,
-    searchAsync
+module.exports = {
+  getAllMovies,
+  getMovieById,
+  createMovie,
+  searchAsync,
 };
