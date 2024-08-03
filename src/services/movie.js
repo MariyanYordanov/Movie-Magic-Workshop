e3e83cf03962488a1b1663736595b137b50d6090
Mariyan Yordanov <marsrewq@gmail.com>
2024-08-03 11:38:32 +0300

    Refactor movie service functions

--- src/services/movie.js
+++ src/services/movie.js
@@ -2,64 +2,68 @@ const mongoose = require("mongoose");
 const { Movie } = require("../models/Movie");
 
 async function getAllMovies() {
-  const movies = await Movie.find().lean();
-  return movies;
+    const movies = await Movie.find().lean();
+    return movies;
 }
 
 async function getMovieById(id) {
-  const movie = await Movie.findOne({ _id: id }).lean().populate("cast");
-  return movie;
+    const movie = await Movie.findOne({ _id: id }).lean();
+    return movie;
 }
 
 async function createMovie(data) {
-  const movie = new Movie();
+    const movie = new Movie();
 
-  movie.title = data.title;
-  movie.genre = data.genre;
-  movie.director = data.director;
-  movie.year = data.year;
-  movie.imageURL = data.imageURL || '/img/logo.png';
-  movie.rating = data.rating;
-  movie.description = data.description;
-  movie.cast = data.cast || [];
+    movie.title = data.title;
+    movie.genre = data.genre;
+    movie.director = data.director;
+    movie.year = data.year;
+    movie.imageURL = data.imageURL;
+    movie.rating = data.rating;
+    movie.description = data.description;
+    movie.cast = [];
 
-  await movie.save();
-  return movie;
+    await movie.save();
+    return movie;
 }
 
 async function searchAsync(title, genre, year) {
-  const movies = await getAllMovies();
-  let result = movies;
+    const movies = await getAllMovies();
+    let result = movies;
 
-  if (title) {
-    result = result.filter((x) => x.title.toLowerCase().includes(title.toLowerCase()));
-  }
+    if (title) {
+        result = result.filter((x) =>
+            x.title.toLowerCase().includes(title.toLowerCase())
+        );
+    }
 
-  if (genre) {
-    result = result.filter((x) => x.genre.toLowerCase().includes(genre.toLowerCase()));
-  }
+    if (genre) {
+        result = result.filter((x) =>
+            x.genre.toLowerCase().includes(genre.toLowerCase())
+        );
+    }
 
-  if (year) {
-    result = result.filter((x) => x.year == year);
-  }
+    if (year) {
+        result = result.filter((x) => x.year == year);
+    }
 
-  return result;
+    return result;
 }
 
 async function createCast(data) {
-  const cast = new Cast();
+    const cast = new Cast();
 
-  cast.name = data.name;
-  cast.character = data.character;
+    cast.name = data.name;
+    cast.character = data.character;
 
-  await cast.save();
-  return cast;
+    await cast.save();
+    return cast;
 }
 
 module.exports = {
-  getAllMovies,
-  getMovieById,
-  createMovie,
-  searchAsync,
-  createCast,
+    getAllMovies,
+    getMovieById,
+    createMovie,
+    searchAsync,
+    createCast,
 };
